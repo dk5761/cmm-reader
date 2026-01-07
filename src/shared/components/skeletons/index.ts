9aafd3bc30155d54790db760efad0b4784633d8f
Darshan Kadam <darshankadam8@gmail.com>
2026-01-08 01:07:01 +0530

    Fix: Resolve Skeleton import path

--- src/shared/components/skeletons/index.ts
+++ src/shared/components/skeletons/index.ts
@@ -1,3 +1,3 @@
-export { Skeleton } from "./Skeleton";
-export { MangaCardSkeleton } from "./skeletons/MangaCardSkeleton";
-export { LibraryGridSkeleton } from "./skeletons/LibrarySkeleton";
+export { Skeleton } from "../Skeleton";
+export { MangaCardSkeleton } from "./MangaCardSkeleton";
+export { LibraryGridSkeleton } from "./LibrarySkeleton";
